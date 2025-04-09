01f47e9786620a49469cd7f597084d57eed862a9
Urkii09 <151901335+alexgoldie09@users.noreply.github.com>
2025-04-09 18:36:17 +1000

    Testing

--- A2Task2LibraryApp/obj/Debug/net8.0/A2Task2LibraryApp.AssemblyInfo.cs
+++ A2Task2LibraryApp/obj/Debug/net8.0/A2Task2LibraryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A2Task2LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd9f2494fcd15150bf152f03a35eb950a23e65c")]
 [assembly: System.Reflection.AssemblyProductAttribute("A2Task2LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A2Task2LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
